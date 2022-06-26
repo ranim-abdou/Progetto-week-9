1fdff1bd003107e0746df6b16f78644a162baaa9
ranim-abdou <104737034+ranim-abdou@users.noreply.github.com>
2022-06-27 00:29:37 +0200

    i

---
+++ ts/script.js
@@ -0,0 +1,57 @@
+"use strict";
+class Smartphone {
+    constructor(nome, ricarica, minuti, chiamate) {
+        this._ricarica = 10;
+        this._minuti = 0;
+        this._chiamate = 0;
+        this._nome = nome;
+        this._ricarica = ricarica;
+        this._minuti = minuti;
+        this._chiamate = chiamate;
+    }
+    get nome() {
+        return this._nome;
+    }
+    get ricarica() {
+        return this._ricarica;
+    }
+    get minuti() {
+        return this._minuti;
+    }
+    get chiamate() {
+        return this._chiamate;
+    }
+    set nome(nome) {
+        this._nome = nome;
+    }
+    set ricarica(ricarica) {
+        this._ricarica = ricarica;
+    }
+    set minuti(minuti) {
+        this._minuti = minuti;
+    }
+    set chiamate(chiamate) {
+        this.chiamate = chiamate;
+    }
+    Ricarica(Ricarica) {
+        this._ricarica = Ricarica;
+    }
+    Minuti(Minuti) {
+        this._minuti = Minuti;
+    }
+    Chiamata(Chiamata) {
+        this._chiamate = Chiamata;
+    }
+    Riparti() {
+        this.chiamate = 0;
+        this._minuti = 0;
+        this._ricarica = 0;
+    }
+    numero404() {
+        this._ricarica - this._minuti * 0.2;
+        return (this._ricarica - this._minuti * 0.2);
+    }
+}
+let utente1 = new Smartphone("Mario", 10, 0, 0);
+let utente2 = new Smartphone("Maria", 10, 0, 0);
+let utente3 = new Smartphone("Genoveffa", 10, 0, 0);
--- ts/script.ts
+++ ts/script.ts
@@ -1,3 +1,66 @@
+class Smartphone {
+    private _nome : string 
+    private _ricarica : number = 10;
+    private _minuti : number = 0;
+    private _chiamate : number = 0;
+
+    constructor ( nome:string, ricarica:number, minuti:number, chiamate:number) {
+        this._nome = nome;
+        this._ricarica = ricarica;
+        this._minuti = minuti;
+        this._chiamate = chiamate;
+    }
+
+    get nome() :string{
+        return this._nome;
+    }
+    get ricarica() :number {
+        return this._ricarica;
+    }
+    get minuti() : number{
+        return this._minuti;
+    }
+    get chiamate():number{
+        return this._chiamate;
+    }
+
+    set nome(nome:string) {
+        this._nome = nome
+    }
+    set ricarica(ricarica:number) {
+        this._ricarica = ricarica
+    }
+    set minuti(minuti:number) {
+        this._minuti = minuti 
+    }
+    set chiamate(chiamate:number) {
+        this.chiamate = chiamate
+    }
+
+    public Ricarica(Ricarica:number): void {
+        this._ricarica = Ricarica
+    }
+
+    public Minuti(Minuti:number): void {
+        this._minuti= Minuti
+    }
+    public Chiamata(Chiamata:number) :void {
+        this._chiamate = Chiamata
+    }
+    public Riparti():void {     //azzera la chiamata 
+        this.chiamate = 0;
+        this._minuti = 0;
+        this._ricarica = 0;
+    }
+    public numero404(): number {
+        this._ricarica - this._minuti*0.2;
+        return (this._ricarica - this._minuti*0.2)
+    }
+}
+
+let utente1 = new Smartphone("Mario", 10, 0, 0);
+let utente2 = new Smartphone("Maria", 10, 0, 0);
+let utente3 = new Smartphone("Genoveffa", 10, 0, 0);
 
 
 
